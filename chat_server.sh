fa3a4542c39deb4fc2d95d22061b2523ce1473e8
Ľubomír Mlích <lubomir.mlich@gmail.com>
2015-07-14 13:44:12 +0200

    base chat server and client

---
+++ chat_server.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Requires: https://github.com/joewalnes/websocketd
+# Chat server, for proper functionality rotation of room logs is nessesary
+#
+# logrotate example
+# cat /etc/logrotate.d/websocketd_chat_server
+# /var/log/websocketd/rooms/*.log {
+#   daily
+#   missingok
+#   rotate 7
+#   compress
+#   delaycompress
+#   notifempty
+#   copytruncate
+# }
+#
+# then apparmor needs permission for this program to write to rooms log directory
+#
+# run with 
+# PORT=8080
+#/path/to/websocketd --port=${PORT} /path/to/chat_server.sh
+
+# variables
+ROOMS_DIR=/var/log/websocketd/rooms
+LAST_MESSAGES=5
+
+# initialization
+echo "Please enter your name:"; read USER
+echo "Please enter room name:"; read ROOM
+ROOM_LOG=${ROOMS_DIR}/${ROOM}.log
+echo "[$(date +%H:%M:%S)] ${USER} joined the ${ROOM}" >> ${ROOM_LOG}
+echo "[$(date +%H:%M:%S)] Welcome to the chat room ${ROOM} ${USER}!"
+
+# logout
+on_die() {
+  echo "[$(date +%H:%M:%S)] ${USER} quit the ${ROOM}" >> ${ROOM_LOG}
+  exit 0
+}
+
+trap 'on_die' TERM SIGHUP SIGINT SIGTERM
+
+# talking
+tail -n ${LAST_MESSAGES} -f ${ROOM_LOG} --pid=$$ &
+while read MSG; do echo "[$(date +%H:%M:%S)] ${USER}> ${MSG}" >> ${ROOM_LOG}; done
+
