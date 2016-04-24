96e69dd62387fceb9dfe293d959e216e67128699
Ľubomír Mlích <lubomir.mlich@gmail.com>
2016-04-24 07:35:57 +0200

    dice number count restriciton, css update

--- parse_command.py
+++ parse_command.py
@@ -14,15 +14,20 @@ from random import randint
 
 def throwDice(x, y, z):
     # throw x y-sided dice then write it to room. If value on the die is greater or equal to z, throw again
+    max_dice_number = 100;
     results = []
+    printinfo = ""
     try:
+         if int(x) > max_dice_number:
+             x = max_dice_number;
+             printinfo = " Max dice number is grater than " + str(max_dice_number) + " throwing only first " + str(max_dice_number) + " dice."
          for a in range(int(x)):
              value = randint(1, int(y))
              while value >= int(z) and int(z) > 1: # need to check if it's possible, that while will end, but what about randomizer?
                  results.append(str(value))
                  value = randint(1,int(y))
              results.append(str(value))
-         print " Throws " + str(x) + " " + str(y) + "-sided dice, throws again on " + str(z) + ": " + str(", ".join(results))
+         print printinfo + " Throws " + str(x) + " " + str(y) + "-sided dice, throws again on " + str(z) + ": " + str(", ".join(results))
     except ValueError:
         print " Bad dice syntax, input is not numeric"
 
@@ -33,7 +38,7 @@ if words[1] == "/dice":
     try:
         throwDice(words[2], words[3], words[4])
     except IndexError:
-        print " Bad dice syntax, usage: /dice number_od_dice number_of_dice_sides target_number_for_throw_again"
+        print " Bad dice syntax, usage: /dice dice_count number_of_dice_sides target_number_for_throw_again"
 elif words[1] == "/color":
     try:
         print " Changed color to " + str(words[2])
